7102ee94caa45500c3ca6ad814c5abaa384e4192
hp-Xcangas\Xavier Cangas <xacr06@gmail.com>
2023-01-08 12:36:23 -0500

    agregar bootstrap  router navbar  y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import Homepage from './pages/Homepage';
+import NavBar from './components/NavBar';
 
 function App() {
+  const footerinfo = {
+    nombre: '© Kruger Corp. 2022. Transforming Enterprises, Boosting Lives.',
+   };
   return (
-    <></>
+    <div className="container">
+    <Router>
+      <div className='nav-tittle'>
+      <NavBar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+      <Footer {...footerinfo} />
+    </Router>
+
+  </div>
   );
 }
 
 export default App;
+
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>Home mensaje de prueba</div>
+  )
+}
+
+export default Homepage
