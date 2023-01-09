8f2e14d12dd9362db4acca9fd25cc921e1567bd8
hp-Xcangas\Xavier Cangas <xacr06@gmail.com>
2023-01-09 03:57:06 -0500

    boooks css

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -6,7 +6,7 @@ const Book = ({item}) => {
   return (
     <div className={styles.bookContainerStyle} >
         <Link className={styles.bookInfoStyle} to={`/view/${item.id}`}>
-            <img src={item.cover} alt={item.title} width = "300px" />
+            <img src={item.cover} alt={item.title} width = "200px" />
             <div>{item.title}</div>
         </Link>
     </div>
